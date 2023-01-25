23ce6a3af0dabce8945fe1c8e9db23ed6182e361
EthanL06 <ethanlanting@gmail.com>
2023-01-25 06:20:37 -0600

    Completed journey

--- components/landing/Journey.tsx
+++ components/landing/Journey.tsx
@@ -43,11 +43,15 @@ const Journey = (props: Props) => {
         {steps.map((step, index) => (
           <div
             key={index}
-            className="flex h-[30vh] flex-col items-center justify-center gap-4 text-center"
+            className="relative flex h-[30vh] flex-col items-center justify-center gap-4 text-center"
           >
-            <div className="flex w-16 items-center justify-center rounded-full bg-light-accent/10 text-4xl font-bold leading-[4rem] text-white">
+            <div className="relative flex w-16 items-center justify-center rounded-full bg-[#32323F] text-center text-4xl font-bold leading-[4rem] text-white">
               {index + 1}
+              {index !== steps.length - 1 && (
+                <div className="absolute top-16 hidden h-[22.88rem] w-1 bg-light-accent/5 md:block" />
+              )}
             </div>
+
             <div className="text-4xl font-bold text-white">{step.title}</div>
             <div className="text-lg text-gray-300">{step.description}</div>
           </div>
